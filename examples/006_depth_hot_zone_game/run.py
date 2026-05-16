35bcf5fd55387feae344fd36ba8fe7a28a09e1db
Michał Maj <michalpmaj@gmail.com>
2026-05-16 11:35:52 +0200

    feat: add depth hot zone game demo

---
+++ examples/006_depth_hot_zone_game/run.py
@@ -0,0 +1,6 @@
+"""Run the OAK-D depth hot zone game demo."""
+
+from oak_vision_lab.demos.depth_hot_zone_game import run_depth_hot_zone_game
+
+if __name__ == "__main__":
+    run_depth_hot_zone_game()
